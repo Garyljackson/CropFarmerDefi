bb890b7e126dae9272599682e4f6e488ee542e10
Gary Jackson <garyljackson@gmail.com>
2021-11-10 08:32:41 +0000

    Move variables up

--- test/CropFarm.test.ts
+++ test/CropFarm.test.ts
@@ -1,22 +1,37 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { Contract, BigNumber } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 describe("Crop Farm", () => {
+  let mockDaiContract: Contract;
+  let cropTokenContract: Contract;
+  let cropFarmContract: Contract;
+
+  let ownerSigner: SignerWithAddress;
+  let account1Signer: SignerWithAddress;
+  let account2Signer: SignerWithAddress;
+
   beforeEach(async () => {
-    const [owner, account1, account2] = await ethers.getSigners();
+    [ownerSigner, account1Signer, account2Signer] = await ethers.getSigners();
 
     const MockDai = await ethers.getContractFactory("ERC20Mock");
     const CropToken = await ethers.getContractFactory("CropToken");
     const CropFarm = await ethers.getContractFactory("CropFarm");
 
-    const mockdai = await MockDai.deploy(
+    mockDaiContract = await MockDai.deploy(
       "MockDai",
       "mDai",
-      owner.address,
+      ownerSigner.address,
       1000
     );
-    const cropToken = await CropToken.deploy();
-    const cropFarm = await CropFarm.deploy(mockdai.address, cropToken.address);
+
+    cropTokenContract = await CropToken.deploy();
+
+    cropFarmContract = await CropFarm.deploy(
+      mockDaiContract.address,
+      cropTokenContract.address
+    );
   });
 
   it("Does Something", async () => {});
